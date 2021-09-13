1cfad5204804da45ec6c5e419b63aa7bbbbc066a
zhangjianke <zhangjianke@wps.cn>
2021-09-13 18:18:30 +0800

    init router

---
+++ src/router.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import VueRouter from "../lib/vue-router/main";
+import Home from "./views/Home";
+import About from "./views/About";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter([
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/about",
+    component: About,
+    children: [
+      {
+        path: "a", // 这里有/ 就是根路径了，不是子路径
+        component: {
+          render: (h) => h("h1", "about A"),
+        },
+      },
+      {
+        path: "b",
+        component: {
+          render: (h) => h("h1", "about B"),
+        },
+      },
+    ],
+  },
+]);
+
+router.beforeEach((from, to, next) => {
+  console.log(1);
+  setTimeout(() => {
+    next();
+  }, 1000);
+});
+
+export default router;
